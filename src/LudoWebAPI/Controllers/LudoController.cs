f36ab4ea12d3383d3880608fb38698c106fd1c32
Joachim Nordeke <joachim@nordeke.se>
2019-01-17 15:57:00 +0100

    Controller changes and yaml edited

--- src/LudoWebAPI/Controllers/LudoController.cs
+++ src/LudoWebAPI/Controllers/LudoController.cs
@@ -8,35 +8,35 @@ namespace LudoWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class LudoController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        // GET api/ludo/newgame
+        [HttpGet("newgame")]
+        public ActionResult<IEnumerable<string>> NewGame()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/ludo/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/ludo
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/ludo/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/ludo/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ src/LudoWebAPI/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LudoGameEngine;
+
+namespace LudoWebAPI.Models
+{
+    public class Game
+    {
+        static List<LudoGame> activeGames = new List<LudoGame> { };
+
+        public static void AddGame(LudoGame game)
+        {
+            activeGames.Add(game);
+        }
+    }
+}
